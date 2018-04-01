bafa4278dd227f60e0ec81a6e985864624e794f4
yuxinwen <yuxinwen@lvmama.com>
2018-04-02 00:09:47 +0800

    runloop

---
+++ RunLoopDemo/FN_Thread.h
@@ -0,0 +1,13 @@
+//
+//  FN_Thread.h
+//  RunLoopDemo
+//
+//  Created by 新闻 on 2018/4/1.
+//  Copyright © 2018年 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FN_Thread : NSThread
+
+@end
---
+++ RunLoopDemo/FN_Thread.m
@@ -0,0 +1,13 @@
+//
+//  FN_Thread.m
+//  RunLoopDemo
+//
+//  Created by 新闻 on 2018/4/1.
+//  Copyright © 2018年 Lvmama. All rights reserved.
+//
+
+#import "FN_Thread.h"
+
+@implementation FN_Thread
+
+@end
